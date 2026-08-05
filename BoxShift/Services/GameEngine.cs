c916c0359e7e0b8c6bb34d7843c92c3c89b57e11
dulran <g00466374@atu.ie>
2026-08-05 17:43:38 +0100

    Added basic GameEngine structure

---
+++ BoxShift/Services/GameEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoxShift.Models;
+
+namespace BoxShift.Services;
+
+public class GameEngine
+{
+    public Level? CurrentLevel { get; private set; }
+
+    public Position PlayerPosition { get; private set; } = new();
+
+    public int MoveCount { get; private set; }
+
+    public void LoadLevel(Level level)
+    {
+        CurrentLevel = level;
+        MoveCount = 0;
+    }
+}
