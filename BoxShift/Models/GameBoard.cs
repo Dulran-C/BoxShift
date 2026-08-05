5972990498854b1d63227c9837527e3c8a94f4ea
dulran <g00466374@atu.ie>
2026-08-05 18:12:38 +0100

    Created GameBoard model and board loading

---
+++ BoxShift/Models/GameBoard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxShift.Models
+{
+    public class GameBoard
+    {
+        public char[,] Grid { get; }
+
+        public int Rows => Grid.GetLength(0);
+
+        public int Columns => Grid.GetLength(1);
+
+        public GameBoard(char[,] grid)
+        {
+            Grid = grid;
+        }
+
+        public char GetCell(int row, int column)
+        {
+            return Grid[row, column];
+        }
+
+        public void SetCell(int row, int column, char value)
+        {
+            Grid[row, column] = value;
+        }
+    }
+}
--- BoxShift/Services/GameEngine.cs
+++ BoxShift/Services/GameEngine.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using BoxShift.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BoxShift.Models;
 
 namespace BoxShift.Services;
 
 public class GameEngine
 {
     public Level? CurrentLevel { get; private set; }
+
+    public GameBoard? Board { get; private set; }
 
     public Position PlayerPosition { get; private set; } = new();
 
@@ -18,6 +20,32 @@ public class GameEngine
     public void LoadLevel(Level level)
     {
         CurrentLevel = level;
+
         MoveCount = 0;
+
+        int rows = level.Rows.Count;
+        int cols = level.Rows.Max(r => r.Length);
+
+        char[,] grid = new char[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                char tile = ' ';
+
+                if (col < level.Rows[row].Length)
+                    tile = level.Rows[row][col];
+
+                grid[row, col] = tile;
+
+                if (tile == '@')
+                {
+                    PlayerPosition = new Position(row, col);
+                }
+            }
+        }
+
+        Board = new GameBoard(grid);
     }
 }
